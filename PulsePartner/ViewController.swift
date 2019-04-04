a729990fa06109a8ba4fcd9cab374951271da193
Ove von Stackelberg <26764163+Ragyal@users.noreply.github.com>
2019-04-04 13:38:10 +0200

    Initial Commit

---
+++ PulsePartner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PulsePartner
+//
+//  Created by Ove von Stackelberg on 04.04.19.
+//  Copyright © 2019 PulsePartner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
