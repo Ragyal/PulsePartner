4981d45c4c0f8e961736dfc5acfdc5844f83dc29
Ove von Stackelberg <26764163+Ragyal@users.noreply.github.com>
2019-05-05 12:46:42 +0200

    SwiftLint

--- PulsePartner/View/Extensions/String.swift
+++ PulsePartner/View/Extensions/String.swift
@@ -11,13 +11,13 @@ import Foundation
 extension String {
     var isValidEmail: Bool {
         let regularExpressionForEmail = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let testEmail = NSPredicate(format:"SELF MATCHES %@", regularExpressionForEmail)
+        let testEmail = NSPredicate(format: "SELF MATCHES %@", regularExpressionForEmail)
         return testEmail.evaluate(with: self)
     }
-    
+
     var isValidPhone: Bool {
         let regularExpressionForPhone = "^\\d{3}-\\d{3}-\\d{4}$"
-        let testPhone = NSPredicate(format:"SELF MATCHES %@", regularExpressionForPhone)
+        let testPhone = NSPredicate(format: "SELF MATCHES %@", regularExpressionForPhone)
         return testPhone.evaluate(with: self)
     }
 }
