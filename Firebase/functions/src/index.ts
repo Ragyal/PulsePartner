3a44a80523c4dd5ea25d23b3c949d32d5a053156
Ove von Stackelberg <26764163+Ragyal@users.noreply.github.com>
2019-06-07 01:53:16 +0200

    Change region to europe and add remove user function

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -1,4 +1,6 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+const firebase = admin.initializeApp();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,8 +9,23 @@ import * as functions from "firebase-functions";
 //  response.send("Hello from Firebase!");
 // });
 
-export const matchUsers = functions.pubsub
+export const matchUsers = functions
+  .region("europe-west1")
+  .pubsub
   .topic("matchUsers")
   .onPublish(async message => {
+    console.log("Matching invoked...")
     return;
+  });
+
+export const removeUser = functions
+  .region("europe-west1")
+  .auth.user()
+  .onDelete(async (user) => {
+    const bucket = firebase.storage().bucket();
+  
+    const asyncUserImageDeletion = bucket.deleteFiles({ prefix: `profilePictures/${user.uid}.png` });
+    const asyncUserDocDeletion = firebase.firestore().doc(`users/${user.uid}`).delete()
+  
+    return Promise.all([asyncUserDocDeletion, asyncUserImageDeletion])
   });
