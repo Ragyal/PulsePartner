1cd871af05844e20607ef1913c2a5c1c82df96ca
Ove von Stackelberg <26764163+Ragyal@users.noreply.github.com>
2019-06-08 03:17:02 +0200

    Implement base structure for match function

--- Firebase/functions/src/index.ts
+++ Firebase/functions/src/index.ts
@@ -10,40 +10,61 @@ const firebase = admin.initializeApp();
 // });
 
 export const removeMatchData = functions
-  .region("europe-west1")
-  .pubsub
-  .topic("removeOldData")
-  .onPublish(async message => {
-    console.log("Garbage collection invoked...")
+.region("europe-west1")
+.pubsub
+.topic("removeOldData")
+.onPublish(async message => {
+  console.log("Garbage collection invoked...")
 
-    const now = admin.firestore.Timestamp.now()
-    const threshold = new admin.firestore.Timestamp(now.seconds - (5*60), 0)
+  const now = admin.firestore.Timestamp.now()
+  const threshold = new admin.firestore.Timestamp(now.seconds - (5*60), 0)
 
-    return firebase.firestore().collection("matchData").where("timestamp", "<=", threshold).get().then(querySnapshot => {
-      querySnapshot.forEach(async doc => {
-        console.log(doc.id)
-        await doc.ref.delete()
-      })
+  return firebase.firestore().collection("matchData").where("timestamp", "<=", threshold).get().then(querySnapshot => {
+    querySnapshot.forEach(async doc => {
+      console.log(doc.id)
+      await doc.ref.delete()
     })
-  });
+  })
+});
+
+
+function doMatch(userA: admin.firestore.QueryDocumentSnapshot, userB: admin.firestore.QueryDocumentSnapshot): Boolean {
+  return true
+}
 
 export const matchUsers = functions
-  .region("europe-west1")
-  .pubsub
-  .topic("matchUsers")
-  .onPublish(async message => {
-    console.log("Matching invoked...")
-    return;
-  });
+.region("europe-west1")
+.pubsub
+.topic("matchUsers")
+.onPublish(async message => {
+  console.log("Matching invoked...")
+  
+  return firebase.firestore().collection("matchData").get().then(querySnapshot => {
+    const docs: admin.firestore.QueryDocumentSnapshot[] = []
+    
+    querySnapshot.forEach(docSnapshot => {
+      docs.push(docSnapshot)
+    })
+    
+    docs.forEach((doc, index, array) => {
+      for (let i = index + 1; i < docs.length; i++) {
+        if (doMatch(doc, docs[i])) {
+          console.log("Match!")
+        }
+      }
+    })
+  })
+});
+
 
 export const removeUser = functions
-  .region("europe-west1")
-  .auth.user()
-  .onDelete(async (user) => {
-    const bucket = firebase.storage().bucket();
-  
-    const asyncUserImageDeletion = bucket.deleteFiles({ prefix: `profilePictures/${user.uid}.png` });
-    const asyncUserDocDeletion = firebase.firestore().doc(`users/${user.uid}`).delete()
-  
-    return Promise.all([asyncUserDocDeletion, asyncUserImageDeletion])
-  });
+.region("europe-west1")
+.auth.user()
+.onDelete(async (user) => {
+  const bucket = firebase.storage().bucket();
+
+  const asyncUserImageDeletion = bucket.deleteFiles({ prefix: `profilePictures/${user.uid}.png` });
+  const asyncUserDocDeletion = firebase.firestore().doc(`users/${user.uid}`).delete()
+
+  return Promise.all([asyncUserDocDeletion, asyncUserImageDeletion])
+});
