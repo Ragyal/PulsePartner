6aecc7351a9d21b20985a64a0b986d42e657627d
Yannik <yannik@trustet.de>
2019-06-20 00:48:44 +0200

    Add comments in LoginViewController

--- PulsePartner/Controller/LoginViewController.swift
+++ PulsePartner/Controller/LoginViewController.swift
@@ -13,8 +13,8 @@ class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailInput: UITextField!
     @IBOutlet weak var passwordInput: UITextField!
-
     @IBOutlet weak var registerHeightContraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let img = UIImage()
@@ -23,6 +23,11 @@ class LoginViewController: UIViewController {
             self.hideKeyboardWhenTappedAround()
     }
 
+    /**
+        Checks if the user is already logged in after the view has appeared
+     - Parameters:
+        - Animated: Specifies whether the change between views is animated
+     */
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         if UserManager.shared.authenticated {
@@ -30,6 +35,11 @@ class LoginViewController: UIViewController {
         }
     }
 
+    /**
+     Call the signIn method in the UserManager to authenticate the user
+     - Parameters:
+        - Sender: Button that was pressed
+     */
     @IBAction func onLoginButtonClick(_ sender: UIButton) {
         UserManager.shared.signIn(withEmail: emailInput.text ?? "",
                                           password: passwordInput.text ?? "",
@@ -39,9 +49,19 @@ class LoginViewController: UIViewController {
             }
         }
     }
+    /**
+     Called when the text field is selected. Calls the function animateViewMoving
+     - Parameters:
+        - Sender: Specifies TextField that was slected
+     */
     @IBAction func mailEditingDidBegin(_ sender: UITextField) {
         animateViewMoving(moveUp: true, moveValue: 140)
     }
+    /**
+     Called if the text field is no longer selected. Calls the function animateViewMoving
+     - Parameters:
+        - Sender: Specifies TextField that was slected
+     */
     @IBAction func mailEditingDidEnd(_ sender: UITextField) {
         animateViewMoving(moveUp: false, moveValue: 140)
     }
@@ -52,6 +72,12 @@ class LoginViewController: UIViewController {
         animateViewMoving(moveUp: false, moveValue: 220)
     }
 
+    /**
+     Moves the view up or down to show the content behind the keyboard
+     - Parameters:
+        - MoveUp: Specifies if the view move up or down.
+        - MoveValue: Specifies how far the view shifts
+     */
     func animateViewMoving (moveUp: Bool, moveValue: CGFloat) {
         let movementDuration: TimeInterval = 0.3
         let movement: CGFloat = ( moveUp ? -moveValue : moveValue)
